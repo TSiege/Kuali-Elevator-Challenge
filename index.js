421be001e6564a20b717a8be5af179d85947d8b1
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-03-17 12:22:55 -0400

    move building to own file

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-// What objects should their be?
-// elevators, yes
-// elevator operator? probs
-// floors? not sure how else to call the operator or elevator?
-// building? does a building have them all?
-// building has floors, elevators, and elevator operator
-// building could instantiate them all
-//   building knows how many floors and elevators
-// floors could call operator,
-// operator sends elevator to floor,
-// elevator goes to floor
-// needs an event loop of some sort
-export class Building {
-  constructor ({ elevatorCount = 4, floors = 20 }) {
+const Building = require('./building')
+const Floor = require('./floor')
+const Elevator = require('./elevator')
+const Operator = require('./operator')
 
-  }
-}
+const building = new Building()
