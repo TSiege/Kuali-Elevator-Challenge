9ecede0a127d9190b20b755197c40cc30431d907
Tristan Siegel <tristan.t.siegel@gmail.com>
2020-03-17 11:54:34 -0400

    flesh out pattern ideas

---
+++ index.js
@@ -0,0 +1,17 @@
+// What objects should their be?
+// elevators, yes
+// elevator operator? probs
+// floors? not sure how else to call the operator or elevator?
+// building? does a building have them all?
+// building has floors, elevators, and elevator operator
+// building could instantiate them all
+//   building knows how many floors and elevators
+// floors could call operator,
+// operator sends elevator to floor,
+// elevator goes to floor
+// needs an event loop of some sort
+class Building {
+  constructor ({ elevatorCount = 4, floors = 20 }) {
+
+  }
+}
